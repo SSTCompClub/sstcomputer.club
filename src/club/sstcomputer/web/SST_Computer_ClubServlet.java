e51b2ee33a7508620ddde7a478a4b745f9e9a8ef
Jasper C. <jaspercreyf@gmail.com>
2015-07-18 19:12:48 -0700

    Grammar, it exists

--- src/club/sstcomputer/web/SST_Computer_ClubServlet.java
+++ src/club/sstcomputer/web/SST_Computer_ClubServlet.java
@@ -1,11 +1,12 @@
 package club.sstcomputer.web;
+
 import java.io.IOException;
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 public class SST_Computer_ClubServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
-	}
+		resp.setContentType("text/plain"); 
+		resp.getWriter().println("Hey there. You've managed to find our servlet. Well, we still yet have to add code to it to make it actually useful.");
+	}	
 }
